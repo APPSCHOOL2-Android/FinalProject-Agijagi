ee92ef5ae8ab8fc456d5104732a6db38b7f0b7a4
hxeyexn <kck85075@kumoh.ac.kr>
2023-08-24 00:16:37 +0900

    Design: CustomProductDetailFragment(사용자) floating button 클릭 리스너 추가 #10

--- agijagi/app/src/main/java/likelion/project/agijagi/product/CustomProductDetailFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/product/CustomProductDetailFragment.kt
@@ -1,20 +1,35 @@
 package likelion.project.agijagi.product
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import likelion.project.agijagi.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import likelion.project.agijagi.databinding.FragmentCustomProductDetailBinding
 
 class CustomProductDetailFragment : Fragment() {
+
+    private var binding: FragmentCustomProductDetailBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_custom_product_detail, container, false)
+        binding = FragmentCustomProductDetailBinding.inflate(inflater)
+
+        clickFloatingButton()
+
+        return binding?.root
+    }
+
+    private fun clickFloatingButton() {
+        binding?.run {
+            customFloatingButtonCustomProductDetail.customFloatingButtonLayout.setOnClickListener {
+                Toast.makeText(context, "Custom Floating Button Clicked!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 
 }
