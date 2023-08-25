b1a6ebc77f200b55c5c5e9529a446753233fa105
jklom0326 <jklom0326@gmail.com>
2023-08-25 16:00:28 +0900

    Fix: color name 변경

--- agijagi/app/src/main/java/likelion/project/agijagi/purchase/PaymentFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/purchase/PaymentFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AutoCompleteTextView
 import android.widget.Button
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import likelion.project.agijagi.R
