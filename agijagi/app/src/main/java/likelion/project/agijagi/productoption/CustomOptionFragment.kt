b77b511ef1a1d9caa09cf31acd356342fcaec086
jklom0326 <jklom0326@gmail.com>
2023-08-25 15:22:33 +0900

    Design: ProductOption 주문 제작 선택 화면 구현 #5

--- agijagi/app/src/main/java/likelion/project/agijagi/productoption/CustomOptionFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/productoption/CustomOptionFragment.kt
@@ -5,16 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import likelion.project.agijagi.MainActivity
 import likelion.project.agijagi.R
+import likelion.project.agijagi.databinding.FragmentCustomOptionBinding
 
 class CustomOptionFragment : Fragment() {
+    lateinit var customOptionBinding: FragmentCustomOptionBinding
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_custom_option, container, false)
+        customOptionBinding = FragmentCustomOptionBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        customOptionBinding.run {
+
+            menuCustomOptionSelectText.setOnItemClickListener { adapterView, view, i, l ->
+                when (i) {
+                    0 -> {
+                        layoutCustomLetteringOption.isVisible = true
+                        layoutCustomPrintOption.isGone = true
+                    }
+                    1 -> {
+                        layoutCustomLetteringOption.isGone = true
+                        layoutCustomPrintOption.isVisible = true
+                    }
+                    2 -> {
+                        layoutCustomLetteringOption.isVisible = true
+                        layoutCustomPrintOption.isVisible = true
+                    }
+                }
+            }
+        }
+
+        return customOptionBinding.root
     }
 
 }
