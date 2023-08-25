9b8d51c820496dcbc9c32805dc8c991deff85b36
SeungHyeon-Hong <49308671+SeungHyeon-Hong@users.noreply.github.com>
2023-08-25 17:21:07 +0900

    Fix: rebase conflict 재작업

--- agijagi/app/src/main/java/likelion/project/agijagi/purchase/PaymentFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/purchase/PaymentFragment.kt
@@ -5,13 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-<<<<<<< HEAD
-import android.widget.AutoCompleteTextView
 import android.widget.Button
 import androidx.navigation.findNavController
-=======
-import android.widget.Button
->>>>>>> develop
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import likelion.project.agijagi.R
 import likelion.project.agijagi.databinding.FragmentPaymentBinding
@@ -39,15 +34,9 @@ class PaymentFragment : Fragment() {
                     }
                     dropdownmenuPaymentCategoryDetail.run {
                         visibility = View.VISIBLE
-<<<<<<< HEAD
-                        (editText as? MaterialAutoCompleteTextView)?.setSimpleItems(R.array.payment_card_detail_category)
-                    }
-                    dropdownTextViewPaymentCategoryDetail.hint = "할부기간"
-=======
                         hint = "할부기간"
                         (editText as? MaterialAutoCompleteTextView)?.setSimpleItems(R.array.payment_card_detail_category)
                     }
->>>>>>> develop
                     editinputlayoutPaymentCategoryDetail.visibility = View.INVISIBLE
                 }
             }
@@ -72,15 +61,10 @@ class PaymentFragment : Fragment() {
                         (editText as? MaterialAutoCompleteTextView)?.setSimpleItems(R.array.payment_phone_category)
                     }
                     dropdownmenuPaymentCategoryDetail.visibility = View.INVISIBLE
-<<<<<<< HEAD
-                    editinputlayoutPaymentCategoryDetail.visibility = View.VISIBLE
-                    editinputPaymentCategoryDetail.hint = "전화번호를 입력하세요."
-=======
                     editinputlayoutPaymentCategoryDetail.run {
                         visibility = View.VISIBLE
                         hint = "전화번호를 입력하세요."
                     }
->>>>>>> develop
                 }
             }
 
@@ -92,23 +76,12 @@ class PaymentFragment : Fragment() {
                         (editText as? MaterialAutoCompleteTextView)?.setSimpleItems(R.array.payment_bank_transfer_category)
                     }
                     dropdownmenuPaymentCategoryDetail.visibility = View.INVISIBLE
-<<<<<<< HEAD
-                    editinputlayoutPaymentCategoryDetail.visibility = View.VISIBLE
-                    editinputPaymentCategoryDetail.hint = "계좌번호를 입력하세요."
-                }
-            }
-
-            buttonPaymentPayment.setOnClickListener {
-                it.findNavController().navigate(R.id.action_paymentFragment_to_purchaseCompleteFragment)
-            }
-=======
                     editinputlayoutPaymentCategoryDetail.run {
                         visibility = View.VISIBLE
                         hint = "계좌번호를 입력하세요."
                     }
                 }
             }
->>>>>>> develop
         }
 
         return fragmentPaymentBinding.root
@@ -125,11 +98,7 @@ class PaymentFragment : Fragment() {
 
         for (button in buttons) {
             if (button == selectedButton) {
-<<<<<<< HEAD
-                button.setTextColor(resources.getColor(R.color.hint_color)) // 선택된 버튼 스타일 변경
-=======
                 button.setTextColor(resources.getColor(R.color.jagi_hint_color)) // 선택된 버튼 스타일 변경
->>>>>>> develop
                 button.setBackgroundResource(R.drawable.narrow_box_rounded_button_solid)
             } else {
                 button.setTextColor(resources.getColor(R.color.jagi_brown))
