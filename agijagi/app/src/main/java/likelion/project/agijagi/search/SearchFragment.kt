0f3a482895c24477377ddde473366bbf88d20157
hxeyexn <kck85075@kumoh.ac.kr>
2023-08-27 23:56:07 +0900

    Design: 검색바, 최근 검색어 UI 구현

--- agijagi/app/src/main/java/likelion/project/agijagi/search/SearchFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/search/SearchFragment.kt
@@ -5,16 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import likelion.project.agijagi.R
+import likelion.project.agijagi.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    lateinit var searchAdapter: SearchAdapter
+
+    companion object {
+        val recentSearchesList =
+            mutableListOf("그릇", "밥그릇", "컵", "접시", "아기자기", "커스텀 도자기", "custom", "레터링", "Agijagi")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater)
+        searchAdapter = SearchAdapter()
+
+        setRecyclerViewRecentSearches()
+
+        return _binding?.root
     }
 
+    private fun setRecyclerViewRecentSearches() {
+        _binding?.run {
+            recyclerviewSearchRecentSearches.run {
+                adapter = searchAdapter
+                layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            }
+        }
+        searchAdapter.submitList(recentSearchesList)
+    }
 }
