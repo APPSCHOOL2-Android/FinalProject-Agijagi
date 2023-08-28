af1fc76c36ac53c4782b0172d8d3662e4b8390ff
hxeyexn <kck85075@kumoh.ac.kr>
2023-08-28 15:54:50 +0900

    Design: 검색 화면 검색 결과 UI 구현 #33

---
+++ agijagi/app/src/main/java/likelion/project/agijagi/search/SearchResultAdapter.kt
@@ -0,0 +1,65 @@
+package likelion.project.agijagi.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import likelion.project.agijagi.databinding.ItemSearchSearchResultBinding
+
+class SearchResultAdapter :
+    ListAdapter<SearchResultModel, SearchResultAdapter.SearchResultViewHolder>(diffUtil) {
+
+    inner class SearchResultViewHolder(private val binding: ItemSearchSearchResultBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: SearchResultModel) {
+
+            binding.run {
+                Glide.with(itemView)
+                    .load(item.prodImage)
+                    .into(binding.imageviewSearchSearchResultImage)
+                textviewSearchSearchResultBrand.text = item.prodBrand
+                textviewSearchSearchResultName.text = item.prodName
+                textviewSearchSearchResultPrice.text = item.prodPrice
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SearchResultViewHolder {
+        val itemSearchSearchResultBinding = ItemSearchSearchResultBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        val viewHolder = SearchResultViewHolder(itemSearchSearchResultBinding)
+
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: SearchResultViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<SearchResultModel>() {
+            override fun areItemsTheSame(
+                oldItem: SearchResultModel,
+                newItem: SearchResultModel
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: SearchResultModel,
+                newItem: SearchResultModel
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
