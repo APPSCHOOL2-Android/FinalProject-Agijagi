6504d53a6ab27aad2a9eb1fd8064d86b4ed475b7
hxeyexn <kck85075@kumoh.ac.kr>
2023-08-29 10:33:55 +0900

    Design: 홈 화면 연결 #56 툴바: 검색 -> 검색화면, 장바구니 -> 장바구니 화면 주문제작상품보기 버튼 -> 주문제작상품 카테고리 화면 menu_homefragment.xml 컨벤션 따라 파일 이름 변경 fragment_home.xml, menu_home.xml 컨벤션 따라 view id 변경

--- agijagi/app/src/main/java/likelion/project/agijagi/home/HomeFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/home/HomeFragment.kt
@@ -1,23 +1,51 @@
 package likelion.project.agijagi.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import likelion.project.agijagi.R
+import likelion.project.agijagi.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
+    private lateinit var fragmentHomeBinding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        fragmentHomeBinding = FragmentHomeBinding.inflate(inflater)
 
+        toolbarMenuItemScreenNavigate()
+        orderMadeProductScreenNavigate()
+
+        return fragmentHomeBinding.root
 
     }
 
+    private fun toolbarMenuItemScreenNavigate() {
+        fragmentHomeBinding.toolbarHome.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.menu_home_search -> {
+                    findNavController().navigate(R.id.action_homeFragment_to_searchFragment)
+                }
+
+                R.id.shoppingbag -> {
+                    findNavController().navigate(R.id.action_homeFragment_to_shoppingListFragment)
+                }
+            }
+            false
+        }
+    }
+
+    private fun orderMadeProductScreenNavigate() {
+        fragmentHomeBinding.linearlayoutHomeToOrderMadeCategory.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_orderMadeCategoryFragment)
+        }
+    }
 }
