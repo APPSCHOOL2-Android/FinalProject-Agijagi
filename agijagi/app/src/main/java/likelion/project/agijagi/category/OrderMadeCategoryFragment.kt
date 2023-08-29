6d68c49125b480e6a644a7239a1be4d5c74a23a1
hxeyexn <kck85075@kumoh.ac.kr>
2023-08-29 16:10:33 +0900

    Design: Order Made Category 화면 연결 #56 툴바 : 백버튼 -> 카테고리 화면, 장바구니 -> 장바구니 화면 Order Made Category 메뉴 -> All, Plate, Cup, Bowl 화면 fragment_order_made_category.xml 컨벤션 따라 id 수정

--- agijagi/app/src/main/java/likelion/project/agijagi/category/OrderMadeCategoryFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/category/OrderMadeCategoryFragment.kt
@@ -1,20 +1,60 @@
 package likelion.project.agijagi.category
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import likelion.project.agijagi.R
+import likelion.project.agijagi.databinding.FragmentOrderMadeCategoryBinding
 
 class OrderMadeCategoryFragment : Fragment() {
+
+    lateinit var fragmentOrderMadeCategoryBinding: FragmentOrderMadeCategoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_made_category, container, false)
+    ): View {
+        fragmentOrderMadeCategoryBinding = FragmentOrderMadeCategoryBinding.inflate(inflater)
+
+        toolbarItemScreenNavigate()
+        setOrderMadeCategoryMenuListener()
+
+        return fragmentOrderMadeCategoryBinding.root
+    }
+
+    private fun toolbarItemScreenNavigate() {
+        fragmentOrderMadeCategoryBinding.toolbarCategory.run {
+            setNavigationOnClickListener {
+                it.findNavController()
+                    .navigate(R.id.action_orderMadeCategoryFragment_to_categoryFragment)
+            }
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.menu_category_shopping_list -> {
+                        findNavController().navigate(R.id.action_orderMadeCategoryFragment_to_shoppingListFragment)
+                    }
+                }
+                false
+            }
+        }
+    }
+
+    private fun setOrderMadeCategoryMenuListener() {
+        fragmentOrderMadeCategoryBinding.run {
+            linearlayoutOrderMadeCategoryButtonOne.setOnClickListener { it.navigateToCategoryDetail() }
+            linearlayoutOrderMadeCategoryButtonTwo.setOnClickListener { it.navigateToCategoryDetail() }
+            linearlayoutOrderMadeCategoryButtonThree.setOnClickListener { it.navigateToCategoryDetail() }
+            linearlayoutOrderMadeCategoryButtonFour.setOnClickListener { it.navigateToCategoryDetail() }
+        }
+    }
+
+    private fun View.navigateToCategoryDetail() {
+        fragmentOrderMadeCategoryBinding.run {
+            findNavController().navigate(R.id.action_orderMadeCategoryFragment_to_categoryDetailFragment)
+        }
     }
 
 }
