384634b076fac94a05cea924544015fe17b246d0
june0103 <rhwns0103@naver.com>
2023-08-30 17:32:48 +0900

    Design: 배송지관리 UI #43 배송지관리 목록 화면 배송지 추가 화면 리사이클러뷰 마지막 아이템 BottomMargin적용

--- agijagi/app/src/main/java/likelion/project/agijagi/buyermypage/ShippingManagementFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/buyermypage/ShippingManagementFragment.kt
@@ -1,20 +1,83 @@
 package likelion.project.agijagi.buyermypage
 
+import android.graphics.Rect
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import likelion.project.agijagi.R
+import likelion.project.agijagi.buyermypage.adapter.ShippingManagementAdapter
+import likelion.project.agijagi.buyermypage.model.ShippingManagementModel
+import likelion.project.agijagi.databinding.FragmentShippingManagementBinding
 
 class ShippingManagementFragment : Fragment() {
+
+    private var _binding: FragmentShippingManagementBinding? = null
+    private val binding get() = _binding!!
+    lateinit var shippingManagementAdapter: ShippingManagementAdapter
+
+    val dataList = arrayListOf<ShippingManagementModel>().apply{
+        add(ShippingManagementModel("집","010-1111-2222","서울시 마포구"))
+        add(ShippingManagementModel("회사","1577-1577", "경기도 고양시"))
+        add(ShippingManagementModel("어느곳","070-1577-1577", "경기도 용인시"))
+        add(ShippingManagementModel("다른수령지","010-4684-1577", "서울"))
+        add(ShippingManagementModel("완전 다른수령지","010-6342-8674", "서울시 어디구"))
+        add(ShippingManagementModel("또 다른수령지","010-8549-1577", "서울"))
+        add(ShippingManagementModel("그리고 완전 다른수령지","010-2454-8674", "서울시 어디구"))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shipping_management, container, false)
+        _binding = FragmentShippingManagementBinding.inflate(inflater)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        shippingManagementAdapter = ShippingManagementAdapter()
+
+        binding.run {
+            recyclerviewShippingManagement.run {
+                adapter = shippingManagementAdapter
+                layoutManager = LinearLayoutManager(context)
+
+                addItemDecoration(MarginItemDecoration(40))
+            }
+
+            // dataList가 비어있을 때 보이도록
+            if (dataList.isEmpty()) {
+                textViewShippingManagemnetNull.visibility = View.VISIBLE
+            } else {
+                textViewShippingManagemnetNull.visibility = View.GONE
+            }
+
+            shippingManagementAdapter.submitList(dataList)
+        }
+    }
+
+    inner class MarginItemDecoration(private val spaceSize: Int) : RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(
+            outRect: Rect, view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State
+        ) {
+            val position = parent.getChildAdapterPosition(view)
+            val itemCount = state.itemCount
+
+            // px -> dp 변환
+            val transformationDP = resources.displayMetrics.density
+            val size = (spaceSize * transformationDP).toInt()
+
+            with(outRect) {
+                bottom = if (position == itemCount - 1) size else 0
+            }
+        }
+    }
 }
---
+++ agijagi/app/src/main/java/likelion/project/agijagi/buyermypage/model/ShippingManagementModel.kt
@@ -0,0 +1,7 @@
+package likelion.project.agijagi.buyermypage.model
+
+data class ShippingManagementModel(
+    val title:String,
+    val phone:String,
+    val address:String
+)
