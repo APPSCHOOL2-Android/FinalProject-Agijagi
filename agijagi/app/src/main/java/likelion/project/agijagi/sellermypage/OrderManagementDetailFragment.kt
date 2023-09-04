485206e28a09d0f26692da95ba82d5ae182d3e88
jklom0326 <jklom0326@gmail.com>
2023-09-04 14:16:21 +0900

    Design: 배송 관리/ 조회 UI #60

--- agijagi/app/src/main/java/likelion/project/agijagi/sellermypage/OrderManagementDetailFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/sellermypage/OrderManagementDetailFragment.kt
@@ -6,16 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import likelion.project.agijagi.R
+import likelion.project.agijagi.databinding.FragmentOrderManagementDetailBinding
+import likelion.project.agijagi.sellermypage.model.OrderManagementModel
+import likelion.project.agijagi.shopping.ShoppingListModel
 
 
 class OrderManagementDetailFragment : Fragment() {
+    private var _binding: FragmentOrderManagementDetailBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_management_detail, container, false)
+    ): View {
+        _binding = FragmentOrderManagementDetailBinding.inflate(inflater)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (arguments?.getParcelable<OrderManagementModel>("orderManagementDetail") != null) {
+            val item = arguments?.getParcelable<OrderManagementModel>("orderManagementDetail")
+            val name = item?.name
+            val price = item?.price
+
+            binding.run {
+                textviewOrderDetailName.text = name
+                textviewOrderDetailPrice.text = price
+            }
+
+        }
+    }
 }
--- agijagi/app/src/main/java/likelion/project/agijagi/sellermypage/OrderManagementFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/sellermypage/OrderManagementFragment.kt
@@ -5,16 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import likelion.project.agijagi.MainActivity
 import likelion.project.agijagi.R
+import likelion.project.agijagi.databinding.FragmentOrderManagementBinding
+import likelion.project.agijagi.sellermypage.adapter.OrderManagementAdapter
+import likelion.project.agijagi.sellermypage.model.OrderManagementModel
+import likelion.project.agijagi.shopping.ShoppingListModel
 
 class OrderManagementFragment : Fragment() {
+    private var _binding: FragmentOrderManagementBinding? = null
+    private val binding get() = _binding!!
+    lateinit var orderManagementAdapter: OrderManagementAdapter
+
+    val dataSet = arrayListOf<OrderManagementModel>().apply {
+        add(OrderManagementModel("김자기", "화려한 접시", "2억원"))
+        add(OrderManagementModel("아기자기", "아름다운 접시", "2원"))
+        add(OrderManagementModel("아기자기", "큰접시", "20.000,000원"))
+        add(OrderManagementModel("아기자기", "부서진 접시", "20원"))
+        add(OrderManagementModel("김자기", "아쉬운 접시", "2,001,402,414원"))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_management, container, false)
+    ): View {
+        _binding = FragmentOrderManagementBinding.inflate(inflater)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        orderManagementAdapter = OrderManagementAdapter(itemClick = { item ->
+            val bundle = Bundle().apply {
+                putParcelable("orderManagementDetail", item)
+            }
+            view.findNavController().navigate(
+                R.id.action_orderManagementFragment_to_orderManagementDetailFragment,
+                bundle
+            )
+        })
+        binding.run {
+            recyclerviewOrderManagement.run {
+                layoutManager = LinearLayoutManager(context)
+                adapter = orderManagementAdapter
+            }
+            orderManagementAdapter.submitList(dataSet)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
