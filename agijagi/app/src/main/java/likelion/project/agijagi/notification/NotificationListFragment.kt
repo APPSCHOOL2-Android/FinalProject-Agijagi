51b10be9431730a137ae2ace8100078f154fe631
SeungHyeon-Hong <49308671+SeungHyeon-Hong@users.noreply.github.com>
2023-09-05 17:09:04 +0900

    Rename: Notification 이름 수정 #100
    Notification to NotificationList

--- agijagi/app/src/main/java/likelion/project/agijagi/notification/NotificationListFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/notification/NotificationListFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [NotificationFragment.newInstance] factory method to
+ * Use the [NotificationListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class NotificationFragment : Fragment() {
+class NotificationListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class NotificationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false)
+        return inflater.inflate(R.layout.fragment_notification_list, container, false)
     }
 
     companion object {
@@ -50,7 +50,7 @@ class NotificationFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            NotificationFragment().apply {
+            NotificationListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
