4707102b4eebe046f263896532963b6c5c3f852b
hxeyexn <kck85075@kumoh.ac.kr>
2023-09-06 03:08:01 +0900

    Feat: 판매자 화면 전환 #91 판매자 화면 전환 구현 SellerNotificationSettingFragment 추가 xml id 수정

--- agijagi/app/src/main/java/likelion/project/agijagi/sellermypage/OrderManagementDetailFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/sellermypage/OrderManagementDetailFragment.kt
@@ -1,25 +1,28 @@
 package likelion.project.agijagi.sellermypage
 
+import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import likelion.project.agijagi.R
+import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import likelion.project.agijagi.MainActivity
 import likelion.project.agijagi.databinding.FragmentOrderManagementDetailBinding
 import likelion.project.agijagi.sellermypage.model.OrderManagementModel
-import likelion.project.agijagi.shopping.ShoppingListModel
 
 
 class OrderManagementDetailFragment : Fragment() {
     private var _binding: FragmentOrderManagementDetailBinding? = null
     private val binding get() = _binding!!
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentOrderManagementDetailBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
 
         return binding.root
     }
@@ -34,8 +37,19 @@ class OrderManagementDetailFragment : Fragment() {
             binding.run {
                 textviewOrderDetailName.text = name
                 textviewOrderDetailPrice.text = price
-            }
 
+                buttonOrderReject.setOnClickListener {
+                    // 다이얼로그 커스텀 필요
+                    MaterialAlertDialogBuilder(mainActivity)
+                        .setTitle("주문 거부")
+                        .setMessage("주문을 거부 하시겠습니까?")
+                        .setPositiveButton("확인") { _: DialogInterface, _: Int ->
+
+                        }
+                        .setNegativeButton("취소", null)
+                        .show()
+                }
+            }
         }
     }
 }
