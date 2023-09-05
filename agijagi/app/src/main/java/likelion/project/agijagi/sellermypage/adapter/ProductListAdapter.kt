4285899d97b29866289c83c62ef5245003e82801
June <66195205+june0103@users.noreply.github.com>
2023-09-05 11:16:39 +0900

    Design: 판매상품리스트 UI #81 (#94)
    * Design: product_list fragment,item UI #81
    
    * Design: 판매상품리스트 UI #81
    리사이클러뷰 어댑터 작업
    dropdownMenu 커스텀

---
+++ agijagi/app/src/main/java/likelion/project/agijagi/sellermypage/adapter/ProductListAdapter.kt
@@ -0,0 +1,75 @@
+package likelion.project.agijagi.sellermypage.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import likelion.project.agijagi.MainActivity
+import likelion.project.agijagi.R
+import likelion.project.agijagi.databinding.ItemProductListBinding
+import likelion.project.agijagi.sellermypage.model.ProductListModel
+
+class ProductListAdapter(val context: Context) :
+    ListAdapter<ProductListModel, ProductListAdapter.ProductListViewHolder>(diffUtil) {
+
+    val productStatusArray = context.resources.getStringArray(R.array.product_list_status)
+
+    inner class ProductListViewHolder(val productListBinding: ItemProductListBinding) :
+        RecyclerView.ViewHolder(productListBinding.root) {
+
+        fun bind(item: ProductListModel) {
+            with(productListBinding) {
+                dropdownTextViewProductListStatus.setText(item.state, false)
+                textViewProductListBrand.text = item.brand
+                textViewProductListName.text = item.name
+                textViewProductListPrice.text = item.price
+                textViewProductListDate.text = item.date
+            }
+        }
+
+        init {
+            productListBinding.run {
+                // dropdownMenu customAdapter
+                val arrayAdapter = ArrayAdapter(
+                    (context as MainActivity),
+                    R.layout.item_product_list_dropdown,
+                    productStatusArray
+                )
+                dropdownTextViewProductListStatus.setAdapter(arrayAdapter)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductListViewHolder {
+        val itemProductListBinding =
+            ItemProductListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val viewHolder = ProductListViewHolder(itemProductListBinding)
+
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: ProductListViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<ProductListModel>() {
+            override fun areItemsTheSame(
+                oldItem: ProductListModel,
+                newItem: ProductListModel
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: ProductListModel,
+                newItem: ProductListModel
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
---
+++ agijagi/app/src/main/java/likelion/project/agijagi/sellermypage/model/ProductListModel.kt
@@ -0,0 +1,9 @@
+package likelion.project.agijagi.sellermypage.model
+
+data class ProductListModel(
+    val state: String,
+    val brand: String,
+    val name: String,
+    val price: String,
+    val date: String
+)
