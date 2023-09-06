f2085214e75b4497f1c8628c1cf1a61b376d783e
Sangwan Han <49683156+jklom0326@users.noreply.github.com>
2023-09-06 12:04:06 +0900

    Design: 구매자 알림 설정 #87 (#104)
    * Design: 구매자 알림 설정 #87
    
    * Fix: conflict 해결 #87

--- agijagi/app/src/main/java/likelion/project/agijagi/buyermypage/BuyerNotificationSettingFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/buyermypage/BuyerNotificationSettingFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import likelion.project.agijagi.R
 
-class AlarmManagementFragment : Fragment() {
+class BuyerNotificationSettingFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm, container, false)
+        return inflater.inflate(R.layout.fragment_buyer_notification_setting, container, false)
     }
 
 }
