aa3d869f1d068c829e0a53a3bacf577618a24b4a
hxeyexn <kck85075@kumoh.ac.kr>
2023-09-07 09:21:37 +0900

    Feat: 내 프로필 관리, 내 스토어 관리 화면 전환 #91

--- agijagi/app/src/main/java/likelion/project/agijagi/buyermypage/ShippingUpdateFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/buyermypage/ShippingUpdateFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import likelion.project.agijagi.R
 import likelion.project.agijagi.databinding.FragmentShippingUpdateBinding
 
 class ShippingUpdateFragment : Fragment() {
@@ -24,8 +26,8 @@ class ShippingUpdateFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.run {
-
+        binding.buttonShippingUpdateRegistration.setOnClickListener {
+            findNavController().navigate(R.id.action_shippingUpdateFragment_to_shippingManagementFragment)
         }
     }
 
