e16056d05cd7c70359ee051230ede2579ddc7d5d
hxeyexn <kck85075@kumoh.ac.kr>
2023-09-09 20:13:50 +0900

    Rename: JoinDataVo -> SignupModel로 파일명 변경 #150

--- agijagi/app/src/main/java/likelion/project/agijagi/signup/SignupModel.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/signup/SignupModel.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class JoinDataVo(
+data class SignupModel(
     val name: String,
     val nickname: String,
     val email: String,
