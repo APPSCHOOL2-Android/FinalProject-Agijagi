13ed1d61af47d2f8d055f2b2daaa3b3aa5d88f16
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2023-09-12 11:49:04 +0900

    Feat: 검색 중 리스트 수정 #52 (#166)
    * Feat: 최근 검색어 삭제 notifyItemRemoved #52
    
    최근 검색어 삭제 기능 구글 권장 사항따라 notifyItemRemoved 사용
    
    * Rename: SearchAdater -> RecentSearchAdapter로 이름 변경 및 SearchAdapter 추가 #52
    
    * Feat: 검색어 입력 중 화면 수정 #52
    
    * Remove: test 파일 삭제 #52

---
+++ agijagi/app/src/main/java/likelion/project/agijagi/search/adapter/SearchAdapter.kt
@@ -0,0 +1,46 @@
+package likelion.project.agijagi.search.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import likelion.project.agijagi.databinding.ItemSearchBinding
+
+class SearchAdapter : ListAdapter<String, SearchAdapter.SearchViewHolder>(diffUtil) {
+
+    inner class SearchViewHolder(private val binding: ItemSearchBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(search: String) {
+            binding.textviewSearch.text = search
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        val binding = ItemSearchBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return SearchViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<String>() {
+            override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+}
