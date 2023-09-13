7e8c6848575ba33aaeba899e24440af92ea10df4
hxeyexn <kck85075@kumoh.ac.kr>
2023-09-14 01:55:14 +0900

    Fix: Essential 파일 내 db 타입 수정 #190

--- agijagi/app/src/main/java/likelion/project/agijagi/Essential.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/Essential.kt
@@ -1,6 +1,7 @@
 package likelion.project.agijagi
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -8,9 +9,13 @@ import java.util.Locale
 
 class Essential {
 
+    // ** 의논 **
+    // Android 컨텍스트 클래스를 정적(static) 필드에 저장하면 메모리 누수(memory leak)의 위험
+    // 메모리 누수는 앱이 메모리를 지속적으로 소비하여 앱 성능을 저하시키고 최종적으로 앱 충돌을 유발할 수 있는 심각한 문제...?
+
     companion object {
         lateinit var mainActivity: MainActivity
-        lateinit var db: Firebase
+        lateinit var db: FirebaseFirestore   // ** 의논 **
         lateinit var auth: FirebaseAuth
         fun getMilliSec(): String {
             val sdf = SimpleDateFormat("yyMMddhhmmssSSS", Locale.getDefault())
