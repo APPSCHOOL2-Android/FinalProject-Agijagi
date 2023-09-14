3997e36536c9582b0cb455bdf739cd9b84594e17
June <66195205+june0103@users.noreply.github.com>
2023-09-14 14:21:58 +0900

    Feat: 기본배송지 설정 #157 (#197)
    DB설계 수정으로 인한 코드 변경
    model 수정
    기본배송지 설정 코드

--- agijagi/app/src/main/java/likelion/project/agijagi/buyermypage/model/ShippingManagementModel.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/buyermypage/model/ShippingManagementModel.kt
@@ -4,7 +4,6 @@ data class ShippingManagementModel(
     val uid: String,
     val address: String,
     val addressDetail: String,
-    val basic : Boolean,
     val title : String,
     val recipientPhone : String,
     val recipient : String
