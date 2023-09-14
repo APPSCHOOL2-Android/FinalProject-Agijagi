61ffe0c8444522d8f79832c7a797d5d47e364626
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2023-09-15 01:18:13 +0900

    Feat: 판매자 상품 등록 -> 상품 상세 미리보기 프로세스 데이터 전달 및 수신 #176 (#203)
    * Feat: 이미지 크기 제한 #176
    
    * Feat: 데이터 번들 제작 #176
    
    * Feat: ProductAdd->ProductDetailPreview로 전달된 bundle 받기 #176
    
    ---------
    
    Co-authored-by: SeungHyeon-Hong <49308671+SeungHyeon-Hong@users.noreply.github.com>

--- agijagi/app/src/main/java/likelion/project/agijagi/model/ProductModel.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/model/ProductModel.kt
@@ -1,18 +1,24 @@
 package likelion.project.agijagi.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class ProductModel(
-    var productId: String,
-    var brand: String,
-    var category: String,
-    var customOptionInfo: Map<String, Any>,
-    var detail: String,
-    var floorPlan: ArrayList<String>,
-    var image: ArrayList<String>,
-    var isCustom: Boolean,
-    var name: String,
-    var outOfStock: Boolean,
-    var price: String,
-    var salesQuantity: Long,
-    var sellerId: String,
-    var updateDate: String
-)
+    var productId: String, // 상품 Pk
+    var brand: String, // 상호명
+    var category: String, // 카테고리
+    var customOptionInfo: HashMap<String, String>, // lettering_fee, lettering_is_use, image_fee, image_is_use
+    var detail: String, // 상품 설명
+    var floorPlan: ArrayList<String>, // 도면 Uri <=4
+    var image: ArrayList<String>, // 사진 Uri <=6
+    var isCustom: Boolean, // 커스텀 여부
+    var name: String, // 상품 이름
+    var outOfStock: Boolean, // 재고 여부
+    var price: String, // 가격
+    var salesQuantity: Long, // 팔린 수량
+    var sellerId: String, // 판매자 Pk
+    var thumbnail_image: String, // 대표 사진 Uri
+    var updateDate: String // yyMMddhhmmsssss
+) : Parcelable
+
