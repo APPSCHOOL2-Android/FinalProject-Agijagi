69ec5f8c015d5881b6143ab0f421c32389ecec27
hxeyexn <kck85075@kumoh.ac.kr>
2023-09-15 10:33:35 +0900

    Feat: 상품 데이터 DB 등록 기능 구현 #204

--- agijagi/app/src/main/java/likelion/project/agijagi/sellermypage/ProductDetailPreviewFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/sellermypage/ProductDetailPreviewFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import likelion.project.agijagi.R
 import likelion.project.agijagi.UserEssential.Companion.db
 import likelion.project.agijagi.UserEssential.Companion.getMillisec
@@ -22,41 +24,7 @@ class ProductDetailPreviewFragment : Fragment() {
 
     lateinit var product: ProductModel
     val dec = DecimalFormat("#,###")
-
-    val customOptionInfo = hashMapOf(
-        "image_fee" to 5000,
-        "image_is_use" to true,
-        "lettering_fee" to 0,
-        "lettering_is_use" to false
-    )
-
-    val productMap = hashMapOf(
-        "brand" to "브랜드명2",
-        "category" to "카테고리명2",
-        "customOptionInfo" to customOptionInfo,
-        "detail" to "상세내용상세내용상세내용상세내용상세내용상세내용상세내용",
-        "floor_plan" to arrayListOf(
-            "floor_plan/aaaa",
-            "floor_plan/bbbb",
-            "floor_plan/cccc",
-            "floor_plan/dddd"
-        ),
-        "image" to arrayListOf(
-            "image/eeee",
-            "image/ffff",
-            "image/gggg",
-            "image/hhhh",
-            "image/iiii",
-            "image/jjjj"
-        ),
-        "is_custom" to true,
-        "name" to "상품명2",
-        "out_of_stock" to false,
-        "price" to 40000,
-        "sales_quantity" to 60,
-        "seller_id" to "dsd45s6dfw55543fe",
-        "update_date" to getMillisec()
-    )
+    val db = Firebase.firestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -106,7 +74,42 @@ class ProductDetailPreviewFragment : Fragment() {
     }
 
     private fun registerProductData() {
-        db = FirebaseFirestore.getInstance()
+        val customOptionInfo = hashMapOf(
+            "image_fee" to product.customOptionInfo["image_fee"],
+            "image_is_use" to product.customOptionInfo["image_is_use"],
+            "lettering_fee" to product.customOptionInfo["lettering_fee"],
+            "lettering_is_use" to product.customOptionInfo["lettering_is_use"]
+        )
+
+        val productMap = hashMapOf(
+            "brand" to product.brand,
+            "category" to product.category,
+            "customOptionInfo" to customOptionInfo,
+            "detail" to product.detail,
+            "floor_plan" to arrayListOf(
+                "floor_plan/aaaa",
+                "floor_plan/bbbb",
+                "floor_plan/cccc",
+                "floor_plan/dddd"
+            ),
+            "image" to arrayListOf(
+                "image/eeee",
+                "image/ffff",
+                "image/gggg",
+                "image/hhhh",
+                "image/iiii",
+                "image/jjjj"
+            ),
+            "is_custom" to product.isCustom,
+            "name" to product.name,
+            "out_of_stock" to product.outOfStock,
+            "price" to product.price,
+            "sales_quantity" to product.salesQuantity,
+            "seller_id" to product.sellerId,
+            "thumbnail_image" to product.thumbnail_image,
+            "update_date" to getMillisec()
+        )
+
         db.collection("product").document(getMillisec()).set(productMap)
     }
 
