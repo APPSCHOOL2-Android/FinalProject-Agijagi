d19b729614eb5af5c592b290a10e3972b7914f2c
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2023-09-15 15:59:07 +0900

    Refactor: user essential 파일 삭제 및 getMilliSec() 이동 #190 (#223)
    * Refactor: user essential 파일명 및 내용 수정 #190
    
    * Fix: Essential 파일 내 db 타입 수정 #190
    
    * Fix: navigation home 수정 #190
    test 후 navigation_graph home을 변경하지 않아 수정함
    
    * Fix: Typo #190
    
    클래스명 변경으로 인한 typo
    
    * Refactor: data class to object #190
    
    유저정보에 관한 클래스들을 싱글톤 처리
    
    * Remove: Essential.kt
    
    * Feat: getMilliSec() 함수 추가
    
    * Fix: 파일 삭제로 인한 참조 에러 수정
    
    * Fix: getMilliSec() 위치 이동으로 인한 import 수정 #190
    
    ---------
    
    Co-authored-by: SeungHyeon-Hong <49308671+SeungHyeon-Hong@users.noreply.github.com>

--- agijagi/app/src/main/java/likelion/project/agijagi/model/UserModel.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/model/UserModel.kt
@@ -1,15 +1,15 @@
 package likelion.project.agijagi.model
 
-data class UserModel(
-    var uid: String,
-    var email: String,
-    var emailNotif: String,
-    var googleLoginCheck: Boolean,
-    var isSeller: Boolean,
-    var name: String,
-    var newChatCount: Long,
-    var newNotifCount: Long,
-    var password: String,
-    var roleId: String,
-    var smsNotif: Boolean
-)
+object UserModel {
+    var uid: String = ""
+    var email: String = ""
+    var emailNotif: String = ""
+    var googleLoginCheck = false
+    var isSeller = false
+    var name: String = ""
+    var newChatCount = -1
+    var newNotifCount = -1
+    var password: String = ""
+    var roleId: String = ""
+    var smsNotif = false
+}
