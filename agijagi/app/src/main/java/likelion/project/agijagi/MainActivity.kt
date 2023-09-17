d0cfc11c922a5834bb84750ca53738ffc5e28e02
hxeyexn <kck85075@kumoh.ac.kr>
2023-09-18 03:14:38 +0900

    Fix: MainActivity test 함수 import 삭제 #233

--- agijagi/app/src/main/java/likelion/project/agijagi/MainActivity.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/MainActivity.kt
@@ -14,7 +14,6 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.firestoreSettings
 import com.google.firebase.ktx.Firebase
 import likelion.project.agijagi.databinding.ActivityMainBinding
-import likelion.project.agijagi.product.test
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
