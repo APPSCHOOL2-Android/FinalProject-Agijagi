4c342ac075915f3b661778d7a32f70715b9afd9b
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2023-09-18 03:17:37 +0900

    Feat: 기성품 상세 화면 상품 정보 로드 기능 구현 #233  (#237)
    * Feat: 기성품 상세 화면 상품 정보 로드 기능 구현 #233
    
    * Remove: Test 파일 삭제 #233
    
    * Fix: MainActivity test 함수 import 삭제 #233

--- agijagi/app/src/main/java/likelion/project/agijagi/product/ProductDetailFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/product/ProductDetailFragment.kt
@@ -4,16 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.bumptech.glide.Glide
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import io.supercharge.shimmerlayout.ShimmerLayout
 import likelion.project.agijagi.R
 import likelion.project.agijagi.databinding.FragmentProductDetailBinding
+import java.text.DecimalFormat
 
 class ProductDetailFragment : Fragment() {
 
     private var _binding: FragmentProductDetailBinding? = null
     private val binding get() = _binding!!
+
+    val dec = DecimalFormat("#,###")
+
+    val db = Firebase.firestore
+    private val storageRef = Firebase.storage.reference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,9 +39,102 @@ class ProductDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        loadProductDataAndInitViews()
         setToolbarItemAction()
         setFavoriteButton()
         setPurchaseButton()
+    }
+
+    private fun loadProductDataAndInitViews() {
+        binding.run {
+            val shimmerLayoutImages = listOf(
+                shimmerLayoutProductDetailImage1,
+                shimmerLayoutProductDetailImage2,
+                shimmerLayoutProductDetailImage3,
+                shimmerLayoutProductDetailImage4,
+                shimmerLayoutProductDetailImage5,
+                shimmerLayoutProductDetailImage6
+            )
+
+            val imageViews = listOf(
+                imageviewProductDetailImage1,
+                imageviewProductDetailImage2,
+                imageviewProductDetailImage3,
+                imageviewProductDetailImage4,
+                imageviewProductDetailImage5,
+                imageviewProductDetailImage6
+            )
+
+            startShimmerAnimations(shimmerLayoutProductDetailThumbnailImage, shimmerLayoutImages)
+
+            db.collection("product").document("230916014552765").get().addOnSuccessListener {
+                val thumbnailImage = it.getString("thumbnail_image").toString()
+                val brand = it.getString("brand").toString()
+                val name = it.getString("name").toString()
+                val price = it.getString("price").toString()
+                val detail = it.getString("detail").toString()
+                val image = it.get("image") as ArrayList<*>
+
+                storageRef.child(thumbnailImage).downloadUrl.addOnSuccessListener { thumbnailUri ->
+                    shimmerLayoutProductDetailThumbnailImage.stopShimmerAnimation()
+                    Glide.with(this@ProductDetailFragment)
+                        .load(thumbnailUri)
+                        .placeholder(R.drawable.product_detail_default_image)
+                        .into(imageviewProductDetailThumbnailImage)
+
+                    displayProductInfo(brand, name, price, name, detail)
+
+                    loadProductImages(image, shimmerLayoutImages, imageViews)
+                }
+            }
+        }
+    }
+
+    private fun startShimmerAnimations(
+        shimmerLayoutThumbnailImage: ShimmerLayout,
+        shimmerLayoutImages: List<ShimmerLayout>
+    ) {
+        shimmerLayoutThumbnailImage.startShimmerAnimation()
+        shimmerLayoutImages.forEach {
+            it.startShimmerAnimation()
+        }
+    }
+
+    private fun displayProductInfo(
+        brand: String,
+        name: String,
+        price: String,
+        title: String,
+        detail: String
+    ) {
+        binding.run {
+            textviewProductDetailBrand.text = brand
+            textviewProductDetailName.text = name
+            textviewProductDetailPrice.text = price
+            textviewProductDetailInfoTitle.text = title
+            textviewProductDetailInfoDescription.text = detail
+        }
+    }
+
+    private fun loadProductImages(
+        image: ArrayList<*>,
+        shimmerLayoutImages: List<ShimmerLayout>,
+        imageViews: List<ImageView>
+    ) {
+        for (idx in 0 until image.size) {
+            storageRef.child(image[idx].toString()).downloadUrl.addOnSuccessListener { imageUri ->
+                shimmerLayoutImages[idx].stopShimmerAnimation()
+
+                Glide.with(this@ProductDetailFragment)
+                    .load(imageUri)
+                    .placeholder(R.drawable.product_detail_default_image)
+                    .into(imageViews[idx])
+            }
+        }
+
+        imageViews.subList(image.size, imageViews.size).forEach { imageView ->
+            imageView.visibility = View.GONE
+        }
     }
 
     private fun setToolbarItemAction() {
