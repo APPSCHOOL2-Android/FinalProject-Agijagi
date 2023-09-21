7b6bdefda9c26c9995221e5afaa0a7d0cca4bc68
SeungHyeon-Hong <49308671+SeungHyeon-Hong@users.noreply.github.com>
2023-09-21 18:45:26 +0900

    Feat: 기 작업내용 정리 #315
    로그인 체크를 위해 필요했던 기능 제거

--- agijagi/app/src/main/java/likelion/project/agijagi/buyermypage/BuyerMypageFragment.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/buyermypage/BuyerMypageFragment.kt
@@ -2,7 +2,6 @@ package likelion.project.agijagi.buyermypage
 
 import android.content.DialogInterface
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,123 +9,54 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.firestore.ktx.firestoreSettings
-import com.google.firebase.ktx.Firebase
-import likelion.project.agijagi.MainActivity
 import likelion.project.agijagi.R
 import likelion.project.agijagi.databinding.FragmentBuyerMypageBinding
-import likelion.project.agijagi.databinding.FragmentNoLoginMypageBinding
+import likelion.project.agijagi.model.UserModel
 
 class BuyerMypageFragment : Fragment() {
 
-    private var auth: FirebaseAuth? = null
-    private lateinit var db: FirebaseFirestore
-
-    private var _fragmentBuyerMypageBinding: FragmentBuyerMypageBinding? = null
-    private val fragmentBuyerMypageBinding get() = _fragmentBuyerMypageBinding!!
-
-    private var _noLoginMyPageFragmentBinding : FragmentNoLoginMypageBinding? = null
-    private val noLoginMyPageFragmentBinding get() = _noLoginMyPageFragmentBinding!!
-
-    private val user = FirebaseAuth.getInstance().currentUser
-
-    lateinit var mainActivity: MainActivity
-
-    private var _name: String = ""
-    private var email: String = ""
+    private var _binding: FragmentBuyerMypageBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentBuyerMypageBinding.inflate(inflater)
 
-        auth = FirebaseAuth.getInstance()
-        if(user != null) {
-            _fragmentBuyerMypageBinding = FragmentBuyerMypageBinding.inflate(inflater,container,false)
-            mainActivity = activity as MainActivity
-            return fragmentBuyerMypageBinding.root
-        } else {
-            _noLoginMyPageFragmentBinding = FragmentNoLoginMypageBinding.inflate(inflater,container,false)
-            return noLoginMyPageFragmentBinding.root
-        }
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setup()
-        if (view == _noLoginMyPageFragmentBinding?.root) {
-            _noLoginMyPageFragmentBinding?.textviewMyPageLogin?.setOnClickListener {
-                login()
-            }
-        } else {
-            setToolbarMenuItem()
-            setBuyerMyPageMenu()
-            setNameEmail()
-        }
-    }
 
-    private fun setNameEmail() {
-        Log.d("email","현재 email: ${auth?.currentUser?.email.toString()}")
-        db.collection("user").document(auth?.currentUser?.email.toString())
-            .get()
-            .addOnSuccessListener {
-                _name = it.getString("name") ?: ""
-                email = it.getString("email") ?: ""
-                Log.d("textview", "현재 name: $_name")
-                Log.d("textview", "현재 email: $email")
-
-                // 데이터를 가져온 후 TextView 업데이트
-                updateTextViews()
-            }
+        updateTextViews()
+        setToolbarMenuItem()
+        setBuyerMyPageMenu()
     }
 
     private fun updateTextViews() {
-        fragmentBuyerMypageBinding.run {
-            if(user != null) {
-                val providerId = user.providerId
-
-                if (providerId == "google.com") {
-                    val name = "${auth?.currentUser?.displayName.toString()}님 안녕하세요!"
-                    textviewBuyerMyPageName.text = name
-                    textviewBuyerMyPageEmail.text = auth?.currentUser?.email.toString()
-
-                } else if (providerId == "firebase") {
-                    val name = "${_name}님 안녕하세요!"
-                    textviewBuyerMyPageName.text = name
-                    textviewBuyerMyPageEmail.text = email
-                }
-            }
+        binding.run {
+            val name = "${UserModel.name}님 안녕하세요!"
+            textviewBuyerMyPageName.text = name
+            val email = UserModel.email
+            textviewBuyerMyPageEmail.text = email
         }
-    }
-
-    private fun login() {
-        findNavController().navigate(R.id.action_buyerMypageFragment_to_loginFragment)
     }
 
     private fun logout() {
-        if (user != null) {
-            val providerId = user.providerId
-
-            if (providerId == "google.com") {
-                FirebaseAuth.getInstance().signOut()
-                findNavController().navigate(R.id.action_buyerMypageFragment_to_loginFragment)
-
-            } else if (providerId == "firebase") {
-                FirebaseAuth.getInstance().signOut()
-                findNavController().navigate(R.id.action_buyerMypageFragment_to_loginFragment)
-            }
-        }
+        FirebaseAuth.getInstance().signOut()
+        findNavController().navigate(R.id.action_buyerMypageFragment_to_loginFragment)
     }
 
+    // 회원탈퇴
     private fun delete() {
         FirebaseAuth.getInstance().currentUser?.delete()
         findNavController().navigate(R.id.action_buyerMypageFragment_to_loginFragment)
     }
 
     private fun setToolbarMenuItem() {
-        fragmentBuyerMypageBinding.toolbarBuyerMyPage.setOnMenuItemClickListener {
+        binding.toolbarBuyerMyPage.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.menu_buyer_my_page_chat -> {
                     findNavController().navigate(R.id.action_buyerMypageFragment_to_chattingListFragment)
@@ -145,7 +75,7 @@ class BuyerMypageFragment : Fragment() {
     }
 
     private fun setBuyerMyPageMenu() {
-        fragmentBuyerMypageBinding.run {
+        binding.run {
             textviewBuyerMyPageProfileManagement.setOnClickListener {
                 findNavController().navigate(R.id.action_buyerMypageFragment_to_profileManagementFragment)
             }
@@ -159,10 +89,9 @@ class BuyerMypageFragment : Fragment() {
             }
 
             textviewBuyerMyPageLogout.setOnClickListener {
-                // 다이얼로그 커스텀 필요
-                MaterialAlertDialogBuilder(mainActivity)
+                MaterialAlertDialogBuilder(requireContext())
                     .setTitle("로그아웃")
-                    .setMessage("로그아웃 하겠습니다.")
+                    .setMessage("로그아웃 합니다.")
                     .setPositiveButton("확인") { _: DialogInterface, _: Int ->
                         logout()
                     }
@@ -171,10 +100,9 @@ class BuyerMypageFragment : Fragment() {
             }
 
             textviewBuyerMyPageQuit.setOnClickListener {
-                // 다이얼로그 커스텀 필요
-                MaterialAlertDialogBuilder(mainActivity)
+                MaterialAlertDialogBuilder(requireContext())
                     .setTitle("회원탈퇴")
-                    .setMessage("고길동님 정말 떠나실 건가요?\n너무 아쉬워요.")
+                    .setMessage("${UserModel.name}님 정말 떠나실 건가요?\n너무 아쉬워요.")
                     .setPositiveButton("확인") { _: DialogInterface, _: Int ->
                         delete()
                     }
@@ -186,17 +114,7 @@ class BuyerMypageFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _fragmentBuyerMypageBinding = null
-        _noLoginMyPageFragmentBinding = null
-    }
-
-    private fun setup() {
-        db = Firebase.firestore
-
-        val settings = firestoreSettings {
-            isPersistenceEnabled = true
-        }
-        db.firestoreSettings = settings
+        _binding = null
     }
 
 }
