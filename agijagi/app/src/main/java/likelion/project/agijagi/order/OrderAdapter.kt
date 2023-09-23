ffce9db65fd5f978dc058cf34fd70f56b1abf0ff
june0103 <rhwns0103@naver.com>
2023-09-24 02:16:59 +0900

    Feat: 주문.배송 기능 구현 #337 주문배송 정보 데이터 불러오기 DB작업 같은 날짜에 주문한 상품 묶기위해 리사이클러뷰 멀티뷰 적용 채팅, 배송조회 데이터 전송 멀티뷰 적용으로 item_order_date UI 생성 데이터 로딩 shimmer 적용 주문상품 없을때 UI 처리 model 변경으로 파일 삭제

--- agijagi/app/src/main/java/likelion/project/agijagi/order/OrderAdapter.kt
+++ agijagi/app/src/main/java/likelion/project/agijagi/order/OrderAdapter.kt
@@ -1,29 +1,110 @@
 package likelion.project.agijagi.order
 
-import android.content.Context
 import android.content.DialogInterface
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import likelion.project.agijagi.R
 import likelion.project.agijagi.databinding.ItemOrderBinding
+import likelion.project.agijagi.databinding.ItemOrderDateBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class OrderAdapter(var context: Context) :
-    ListAdapter<OrderModel, OrderAdapter.OrderViewHolder>(diffUtil) {
+class OrderAdapter() :
+    ListAdapter<ParentOrderItem, OrderAdapter.OrderViewHolder>(OrderViewHolder.diffUtil) {
 
-    inner class OrderViewHolder(val orderBinding: ItemOrderBinding) :
-        RecyclerView.ViewHolder(orderBinding.root) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
 
-        fun bind(item: OrderModel) {
-            with(orderBinding) {
-                textViewOrderState.text = item.state
-                textViewOrderBrand.text = item.brand
-                textViewOrderName.text = item.name
-                textViewOrderPrice.text = item.price
+        val viewHolder = when (viewType) {
+            0 -> {
+                OrderDataViewHolder(ItemOrderBinding.inflate(layoutInflater, parent, false))
+            }
+
+            1 -> {
+                OrderDateViewHolder(ItemOrderDateBinding.inflate(layoutInflater, parent, false))
+            }
+
+            else -> throw Exception()
+        }
+
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: OrderViewHolder, position: Int) {
+        holder.onBind(currentList[position])
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return currentList[position].viewType
+    }
+
+
+    abstract class OrderViewHolder(private val itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        abstract fun onBind(item: ParentOrderItem)
+
+        companion object {
+            val diffUtil = object : DiffUtil.ItemCallback<ParentOrderItem>() {
+                override fun areItemsTheSame(
+                    oldItem: ParentOrderItem,
+                    newItem: ParentOrderItem
+                ): Boolean {
+                    return oldItem == newItem
+                }
+
+                override fun areContentsTheSame(
+                    oldItem: ParentOrderItem,
+                    newItem: ParentOrderItem
+                ): Boolean {
+                    return oldItem.equals(newItem)
+                }
+            }
+        }
+    }
+
+    class OrderDataViewHolder(val orderBinding: ItemOrderBinding) :
+        OrderViewHolder(orderBinding.root) {
+        override fun onBind(item: ParentOrderItem) {
+            val orderItem = item as OrderDataClass
+            orderBinding.run {
+                Glide.with(orderBinding.root)
+                    .load(orderItem.thumbnailImage)
+                    .into(orderBinding.imageViewOrderProduct)
+                textViewOrderState.text = orderItem.state
+                textViewOrderBrand.text = orderItem.brand
+                textViewOrderName.text = orderItem.name
+                textViewOrderPrice.text = orderItem.price
+                val count = orderItem.count
+                textViewOrderOption.text = "$count 개"
+
+                buttonOrderChat.setOnClickListener {
+                    // buyerId : String , sellerId  : String
+                    val bundle = Bundle().apply {
+                        putString("buyerId", orderItem.buyerId)
+                        putString("sellerId", orderItem.sellerId)
+                    }
+                    it.findNavController()
+                        .navigate(R.id.action_orderFragment_to_chattingListFragment, bundle)
+                }
+
+                buttonOrderShippingTracking.setOnClickListener {
+                    // state, thumbnail_image, name, date
+                    val bundle = Bundle().apply {
+                        putString("state", orderItem.state)
+                        putString("thumbnail_image", orderItem.thumbnailImage)
+                        putString("name", orderItem.name)
+                    }
+                    it.findNavController()
+                        .navigate(R.id.action_orderFragment_to_deliveryTrackingFragment, bundle)
+                }
             }
         }
 
@@ -31,7 +112,7 @@ class OrderAdapter(var context: Context) :
             orderBinding.run {
                 buttonOrderExchange.setOnClickListener {
                     // 다이얼로그 커스텀 필요
-                    MaterialAlertDialogBuilder(context)
+                    MaterialAlertDialogBuilder(orderBinding.root.context)
                         .setTitle("교환/환불 신청")
                         .setMessage("교환/환불 신청 하시겠습니까?")
                         .setPositiveButton("확인") { _: DialogInterface, _: Int ->
@@ -40,48 +121,28 @@ class OrderAdapter(var context: Context) :
                         .setNegativeButton("취소", null)
                         .show()
                 }
-
-                buttonOrderShippingTracking.setOnClickListener {
-                    it.findNavController()
-                        .navigate(R.id.action_orderFragment_to_deliveryTrackingFragment)
-                }
-
-                buttonOrderChat.setOnClickListener {
-                    it.findNavController()
-                        .navigate(R.id.action_orderFragment_to_chattingListFragment)
-                }
             }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderViewHolder {
-        val itemOrderBinding =
-            ItemOrderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        val viewHolder = OrderViewHolder(itemOrderBinding)
+    class OrderDateViewHolder(val orderDateBinding: ItemOrderDateBinding) :
+        OrderViewHolder(orderDateBinding.root) {
+        override fun onBind(item: ParentOrderItem) {
+            val orderDate = item as OrderDateClass
+            orderDateBinding.run {
+                val dateFormat1 = SimpleDateFormat(
+                    "yyMMdd",
+                    Locale.getDefault()
+                )
+                val dateFormat2 =
+                    SimpleDateFormat("yy. M. d", Locale.getDefault())
+                val dateData = orderDate.date
+                val parseData = dateFormat1.parse(dateData)
 
-        return viewHolder
-    }
-
-    override fun onBindViewHolder(holder: OrderViewHolder, position: Int) {
-        holder.bind(currentList[position])
-    }
-
-    companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<OrderModel>() {
-            override fun areItemsTheSame(
-                oldItem: OrderModel,
-                newItem: OrderModel
-            ): Boolean {
-                return oldItem == newItem
-            }
-
-            override fun areContentsTheSame(
-                oldItem: OrderModel,
-                newItem: OrderModel
-            ): Boolean {
-                return oldItem == newItem
+                // yy/MM/dd 형식의 Date
+                val orderFormatDate = dateFormat2.format(parseData)
+                textViewOrderDate.text = orderFormatDate
             }
         }
     }
-
 }
